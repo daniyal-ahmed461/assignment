16c2b634ff9c6bc52925a72d89af7d6dd04b4ac3
Daniyal Ahmed <daniyalxfalcon@gmail.com>
2024-08-12 18:27:10 +0500

    assignment

---
+++ app.js
@@ -0,0 +1,55 @@
+alert ("Welcome to Our Website")
+var password=prompt("enter password")
+if (password==="12345") {
+    alert("welcome")
+}
+else {
+    alert("Error! Please Enter a valid password")
+}
+alert("Hint : Use line break")
+var username=""
+var myname="Daniyal Ahmed"
+var message="Hello World"
+alert(message)
+var studentname=prompt("your name")
+alert(studentname)
+var email=prompt("your email")
+alert("Hint: use string concatenation")
+var email=prompt("your email")
+console.log("Hint: use string concatenation")
+var age="age = 17"
+alert(age)
+var birthyear="My birthyear is 2009"
+alert(birthyear)
+var visitorname=prompt("visitor's name")
+var producttitle=prompt("Product's title")
+var quantity=prompt("Quantity")
+alert(visitorname + " ordered "+ quantity + " " + producttitle+"(s)" +" on XYZ clothing store")
+var password1="" ; password2="" ; password3=""
+// legal variables name:
+// userName
+// user_name
+// UserName
+// QUESTION_ASKED
+// userName1
+// illegal variables name:
+// user name
+// #username
+// USERNAME
+// var
+// 9username
+var num1=+prompt("enter first digit");
+var num2=+prompt("enter second digit")
+alert(num1 + num2)
+var num1=+prompt("enter first digit");
+var num2=+prompt("enter second digit")
+alert(num1 - num2)
+var num1=+prompt("enter first digit");
+var num2=+prompt("enter second digit")
+alert(num1 * num2)
+var num1=+prompt("enter first digit");
+var num2=+prompt("enter second digit")
+alert(num1 / num2)
+var num1=+prompt("enter first digit");
+var num2=+prompt("enter second digit")
+alert(num1 % num2)
